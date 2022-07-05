3034602632d055e614fa5a20e0d9332c611bc0e4
Ilya <Ilya.adam4yk@gmail.com>
2022-07-05 17:30:02 +0300

    added functional:change sizes and colors

--- scripts/script.js
+++ scripts/script.js
@@ -21,3 +21,30 @@ function column(){
 function columnRev(){
     document.getElementsByClassName('block__container')[0].style.flexDirection='column-reverse'
 }
+
+//Sizes
+function setWidth(){
+    let newWidth=document.getElementsByClassName("newWidth")[0].value
+    let elements=  document.getElementsByClassName('flex_item')
+    for (let i = 0; i < elements.length; i++) {
+    elements[i].style.width=newWidth+'px'
+    }
+}
+
+function setHeight(){
+    let newHeight=document.getElementsByClassName("newHeight")[0].value
+    let elements=  document.getElementsByClassName('flex_item')
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.height=newHeight+'px'
+    }
+}
+
+function getRandomColor(i) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+     document.getElementsByClassName(i)[0].style.backgroundColor=color
+    console.log("set color "+ color+" for "+ i)
+ }
