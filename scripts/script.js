cd271b56070b8b9c818d5d046b29d1ecbf6e26e9
Ilya <Ilya.adam4yk@gmail.com>
2022-07-13 17:13:29 +0300

    Renamed configs tags Added justify-content spaces, renamed buttons of justify content Reworked function checkNull()

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,103 @@
+let block=document.getElementsByClassName('block__container');
+
 //displays
 function withoutFlex(){
-document.getElementsByClassName('block__container')[0].style.display='block'
+block[0].style.display='block'
 }
 function withFlex(){
-    document.getElementsByClassName('block__container')[0].style.display='flex'
+block[0].style.display='flex'
 }
 function inlineFlex(){
-    document.getElementsByClassName('block__container')[0].style.display='inline-flex'
+    block[0].style.display='inline-flex'
 }
 
 //directions
 function row() {
-    document.getElementsByClassName('block__container')[0].style.flexDirection='row'
+block[0].style.flexDirection='row'
 }
-
 function rowRev() {
-    document.getElementsByClassName('block__container')[0].style.flexDirection='row-reverse'
+block[0].style.flexDirection='row-reverse'
 }
-
 function column(){
-    document.getElementsByClassName('block__container')[0].style.flexDirection='column'
+block[0].style.flexDirection='column'
 }
 function columnRev(){
-    document.getElementsByClassName('block__container')[0].style.flexDirection='column-reverse'
+block[0].style.flexDirection='column-reverse'
 }
+
 // wraps
 function wrp(){
-    document.getElementsByClassName('block__container')[0].style.flexWrap="wrap"
+block[0].style.flexWrap="wrap"
 }
 function nowrp(){
-    document.getElementsByClassName('block__container')[0].style.flexWrap='nowrap'
+block[0].style.flexWrap='nowrap'
 }
 function wrp_reverse(){
-    document.getElementsByClassName('block__container')[0].style.flexWrap='wrap-reverse'
+block[0].style.flexWrap='wrap-reverse'
 }
+
+//Alignments
+function justStart(){
+     block[0].style.justifyContent='flex-start'
+}
+function justEnd(){
+    block[0].style.justifyContent='flex-end'
+}
+function justCenter(){
+    block[0].style.justifyContent='center'
+}
+function justSpBetween(){
+    block[0].style.justifyContent="space-between"
+}
+function justSpAround(){
+    block[0].style.justifyContent="space-around"
+}
+function justSpEvenly(){
+    block[0].style.justifyContent="space-evenly"
+}
+
 //Sizes and colors
 function setGap(){
     let newGap=document.getElementsByClassName('newGap')[0].value
+    if (checkNull(newGap)){
+        block[0].style.gap=null
+    }
+    else{
     let select=document.getElementById('gap_type')
     let type=select.options[select.selectedIndex].label
-    document.getElementsByClassName('block__container')[0].style.gap=newGap+type
-
+    block[0].style.gap=newGap+type
+    }
 }
 function setWidth(i,a){
-     let newWidth=document.getElementsByClassName("newWidth")[a].value
+    let newWidth=document.getElementsByClassName("newWidth")[a].value
     let elements= document.getElementsByClassName(i)
     let select=document.getElementById('width_type')
+    if (checkNull(newWidth)){
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width=null
+    }}
+        else{
     let type=select.options[select.selectedIndex].label
     for (let i = 0; i < elements.length; i++) {
     elements[i].style.width=newWidth+type
     }
-}
-
+}}
 function setHeight(i,a){
     let newHeight=document.getElementsByClassName("newHeight")[a].value
     let elements=  document.getElementsByClassName(i)
+    if (checkNull(newHeight)){
+        for (let i = 0; i < elements.length; i++) {
+        elements[i].style.height=null
+    }
+    }else
+    {
     let select=document.getElementById('height_type')
     let type=select.options[select.selectedIndex].label
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.height=newHeight+type
     }
+    }
 }
-
 function getRandomGradient(i) {
     let classElmnt=i.className
     let random=Math.round(Math.random()*(5-2+1)+2)
@@ -72,7 +108,6 @@ function getRandomGradient(i) {
     background=background.slice(0,-2)+')'
       document.getElementsByClassName(classElmnt)[0].style.background=background
 }
-
  function colorGeneration() {
      let letters = '0123456789ABCDEF';
      let color = '#';
@@ -82,3 +117,7 @@ function getRandomGradient(i) {
      return color
  }
 
+ //others
+function checkNull(i) {
+    return (i<0)? true:false
+}
