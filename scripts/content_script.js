9b2dbd2e57a775c955316bf89b2444dd8745157b
Ilya <Ilya.adam4yk@gmail.com>
2022-07-13 12:27:14 +0300

    Added content_script.js Rework system types

---
+++ scripts/content_script.js
@@ -0,0 +1,24 @@
+function includeOptions(){
+    let options=[
+    "px",
+    "%",
+    "ch",
+    "cm",
+    "em",
+    "ex",
+    "gd",
+    "mm",
+    "pt",
+    "rem",
+    "vh"
+    ]
+  let min=0,
+      max=options.length-1,
+    select=document.getElementsByClassName('types');
+    [].forEach.call(select,function (select) {
+        for (let i = min; i <max; i++) {
+        let opt=document.createElement('option');
+            opt.value=options[i];
+            opt.innerHTML=options[i];
+            select.appendChild(opt);
+} } )}
