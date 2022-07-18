34a797c7c5aa58c7b8fa57e8fa2437ce3f5ea540
Ilya <Ilya.adam4yk@gmail.com>
2022-07-18 14:06:43 +0300

    Deleted setGrow function Added includeRates() added function with rates

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,8 +1,8 @@
-function includeOptions(){
-    let options=[
-    "px",
-    "%",
-    "ch",
+function includeOptions() {
+    let options = [
+        "px",
+        "%",
+        "ch",
         "cm",
         "em",
         "ex",
@@ -15,24 +15,45 @@ function includeOptions(){
     let min = 0,
         max = options.length,
         select = document.getElementsByClassName('types');
-    [].forEach.call(select,function (select) {
-        for (let i = min; i <max; i++) {
-        let opt=document.createElement('option');
-            opt.value=options[i];
-            opt.innerHTML=options[i];
+    [].forEach.call(select, function (select) {
+        for (let i = min; i < max; i++) {
+            let opt = document.createElement('option');
+            opt.value = options[i];
+            opt.innerHTML = options[i];
             select.appendChild(opt);
-} } )}
+        }
+    })
+}
 
 function includeItems() {
-    let items=document.getElementsByClassName('flex_item')
+    let items = document.getElementsByClassName('flex_item')
     let min = 0,
         max = items.length,
         select = document.getElementsByClassName('fl_items');
-    [].forEach.call(select,function (select) {
-        for (let i = min; i <max; i++) {
-            let opt=document.createElement('option');
-            opt.value=items.item(i).className;
-            let end=opt.value.search(' ')
-            opt.innerHTML=items.item(i).className.substring('0',end);
+    [].forEach.call(select, function (select) {
+        for (let i = min; i < max; i++) {
+            let opt = document.createElement('option');
+            opt.value = items.item(i).className;
+            let end = opt.value.search(' ')
+            opt.innerHTML = items.item(i).className.substring('0', end);
             select.appendChild(opt);
-}})}
+        }
+    })
+}
+
+function includeRates() {
+    let rate = [
+        'flexShrink', 'flexBasis', 'flexGrow'
+    ]
+    let min = 0,
+        max = rate.length,
+        select = document.getElementsByClassName('fl_rates');
+    [].forEach.call(select, function (select) {
+        for (let i = min; i < max; i++) {
+            let opt = document.createElement('option');
+            opt.value = rate[i];
+            opt.innerHTML = rate[i];
+            select.appendChild(opt);
+        }
+    })
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,101 +1,113 @@
-
-let block=document.getElementsByClassName('block__container');
+let block = document.getElementsByClassName('block__container');
 
 //displays
 function display_set(i) {
-    block[0].style.display=i
+    block[0].style.display = i
 }
 
 //directions
 function direction_set(i) {
-    block[0].style.flexDirection=i
+    block[0].style.flexDirection = i
 }
 
 function wrap_set(i) {
-    block[0].style.flexWrap=i
+    block[0].style.flexWrap = i
 }
 
 //Alignments
 function justifyContent_set(i) {
-    block[0].style.justifyContent=i
+    block[0].style.justifyContent = i
 }
 
 function align_self(i) {
-    let select=document.getElementById('selected_item')
-    let elem=select.options[select.selectedIndex].value
+    let select = document.getElementById('selected_item')
+    let elem = select.options[select.selectedIndex].value
     console.log(elem)
-    document.getElementsByClassName(elem)[0].style.alignSelf=i
+    document.getElementsByClassName(elem)[0].style.alignSelf = i
 }
 
 //Sizes and colors
-function setGap(){
-    let newGap=document.getElementsByClassName('newGap')[0].value
-    if (checkNull(newGap)){
-        block[0].style.gap=null
-    }
-    else{
-    let select=document.getElementById('gap_type')
-    let type=select.options[select.selectedIndex].label
-    block[0].style.gap=newGap+type
-    }
-}
-function setWidth(i,a){
-    let newWidth=document.getElementsByClassName("newWidth")[a].value
-    let elements= document.getElementsByClassName(i)
-    let select=document.getElementById('width_type')
-    if (checkNull(newWidth)){
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.width=null
-    }}
-        else{
-    let type=select.options[select.selectedIndex].label
-    for (let i = 0; i < elements.length; i++) {
-    elements[i].style.width=newWidth+type
-    }
-}}
-function setHeight(i,a){
-    let newHeight=document.getElementsByClassName("newHeight")[a].value
-    let elements=document.getElementsByClassName(i)
-    if (checkNull(newHeight)){
-        for (let i = 0; i < elements.length; i++) {
-        elements[i].style.height=null
-    }
-    }else
-    {
-    let select=document.getElementById('height_type')
-    let type=select.options[select.selectedIndex].label
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.height=newHeight+type
-    }
+function setGap() {
+    let newGap = document.getElementsByClassName('newGap')[0].value
+    if (checkNull(newGap)) {
+        block[0].style.gap = null
+    } else {
+        let select = document.getElementById('gap_type')
+        let type = select.options[select.selectedIndex].label
+        block[0].style.gap = newGap + type
     }
 }
+
+function setWidth(i, a) {
+    let newWidth = document.getElementsByClassName("newWidth")[a].value
+    let elements = document.getElementsByClassName(i)
+    let select = document.getElementById('width_type')
+    if (checkNull(newWidth)) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = null
+        }
+    } else {
+        let type = select.options[select.selectedIndex].label
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = newWidth + type
+        }
+    }
+}
+
+function setHeight(i, a) {
+    let newHeight = document.getElementsByClassName("newHeight")[a].value
+    let elements = document.getElementsByClassName(i)
+    if (checkNull(newHeight)) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.height = null
+        }
+    } else {
+        let select = document.getElementById('height_type')
+        let type = select.options[select.selectedIndex].label
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.height = newHeight + type
+        }
+    }
+}
+
 function getRandomGradient(i) {
-    let classElmnt=i.className
-    let random=Math.round(Math.random()*(5-2+1)+2)
-    let background='linear-gradient(to left,'
-    for (let j = 1; j <=random; j++) {
-        background+=colorGeneration()+', '
+    let classElmnt = i.className
+    let random = Math.round(Math.random() * (5 - 2 + 1) + 2)
+    let background = 'linear-gradient(to left,'
+    for (let j = 1; j <= random; j++) {
+        background += colorGeneration() + ', '
     }
-    background=background.slice(0,-2)+')'
-      document.getElementsByClassName(classElmnt)[0].style.background=background
-}
- function colorGeneration() {
-     let letters = '0123456789ABCDEF';
-     let color = '#';
-     for (let i = 0; i < 6; i++) {
-         color += letters[Math.floor(Math.random() * 16)];
-     }
-     return color
- }
-
-function setGrow() {
-    let select = document.getElementsByClassName("fl_items")[1].value
-    let size = document.getElementsByClassName('newGrow')[0].value
-    if (checkNull(size)) size = 0
-    document.getElementsByClassName(select)[0].style.flexGrow = size
+    background = background.slice(0, -2) + ')'
+    document.getElementsByClassName(classElmnt)[0].style.background = background
 }
 
- //others
+function colorGeneration() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color
+}
+
+function setRates() {
+    let nameRate = document.getElementsByClassName('fl_rates')[0].value;
+    let nameItem = document.getElementsByClassName('fl_items')[1].value;
+    let sizeRate = document.getElementsByClassName('newRate')[0].value;
+    switch (nameRate) {
+        case 'flexGrow':
+            document.getElementsByClassName(nameItem)[0].style.flexGrow = sizeRate
+            break;
+        case 'flexBasis':
+            document.getElementsByClassName(nameItem)[0].style.flexBasis = sizeRate
+            break;
+        case 'flexShrink':
+            document.getElementsByClassName(nameItem)[0].style.flexShrink = sizeRate
+            break;
+    }
+}
+
+//others
 function checkNull(i) {
     return (i < 0)
 }
